f6396a540ada54e8aab3fe775471317d8dc9ab33
mateuscd1 <joaomateusdutra36@gmail.com>
2025-07-29 15:12:37 -0300

    Adicionando novas models e entradas

--- projetosdj/models.py
+++ projetosdj/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
     def __str__(self):
         """Devolve uma representação em string do modelo"""
         return self.text
+
+class Entry(models.Model):
+    """Algo específico aprendido sobre o assunto"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Devolve uma representação em string do modelo"""
+        return self.text[:50] + '...'
